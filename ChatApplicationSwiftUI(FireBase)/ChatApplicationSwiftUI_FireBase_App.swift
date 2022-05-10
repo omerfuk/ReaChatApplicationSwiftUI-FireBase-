24bbc9a02d2c347724dd310bea258e98c3fb552a
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-10 12:41:46 +0300

    Initial Commit

---
+++ ChatApplicationSwiftUI(FireBase)/ChatApplicationSwiftUI_FireBase_App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApplicationSwiftUI_FireBase_App.swift
+//  ChatApplicationSwiftUI(FireBase)
+//
+//  Created by Ömer Faruk Kılıçaslan on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatApplicationSwiftUI_FireBase_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
